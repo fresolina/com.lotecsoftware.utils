f04e4bb0d41e5378219a9018117da8d728fa5721
fresolina <74408325+fresolina@users.noreply.github.com>
2023-03-04 14:37:58 +0100

    Move component to top
    Adds context menu "Move to top" for a component in inspector.

---
+++ Editor/Scripts/MoveComponentTool.cs
@@ -0,0 +1,33 @@
+using UnityEditor;
+using UnityEngine;
+
+public class MoveComponentTool {
+    const string k_menuMoveToTop = "CONTEXT/Component/Move To Top";
+    const string k_menuMoveToBottom = "CONTEXT/Component/Move To Bottom";
+
+    [MenuItem(k_menuMoveToTop, priority = 501)]
+    public static void MoveComponentToTopMenuItem(MenuCommand command) {
+        while (UnityEditorInternal.ComponentUtility.MoveComponentUp((Component)command.context)) ;
+    }
+
+    [MenuItem(k_menuMoveToTop, validate = true)]
+    public static bool MoveComponentToTopMenuItemValidate(MenuCommand command) {
+        if (command.context == null) return false;
+        Component component = ((Component)command.context);
+        Component[] components = component.GetComponents<Component>();
+        return components.Length > 1 && components[1] != component;
+    }
+
+    [MenuItem(k_menuMoveToBottom, priority = 501)]
+    public static void MoveComponentToBottomMenuItem(MenuCommand command) {
+        while (UnityEditorInternal.ComponentUtility.MoveComponentDown((Component)command.context)) ;
+    }
+
+    [MenuItem(k_menuMoveToBottom, validate = true)]
+    public static bool MoveComponentToBottomMenuItemValidate(MenuCommand command) {
+        if (command.context == null) return false;
+        Component component = ((Component)command.context);
+        Component[] components = component.GetComponents<Component>();
+        return components.Length > 1 && components[components.Length - 1] != component;
+    }
+}
