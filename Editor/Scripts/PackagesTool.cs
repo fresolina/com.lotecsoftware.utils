38973b6e48464385b6fae558970f4fc4422cc2ef
fresolina <74408325+fresolina@users.noreply.github.com>
2023-03-05 09:48:34 +0100

    Improve packages cleanup
    Filter packages requests so we only work with what we want.

--- Editor/Scripts/PackagesTool.cs
+++ Editor/Scripts/PackagesTool.cs
@@ -1,13 +1,17 @@
+using System.Linq;
 using UnityEditor;
 using UnityEditor.PackageManager;
 using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 
 namespace Lotec.Utils.Packages {
+    /// <summary>
+    /// Clean up unwanted packages.
+    /// </summary>
     static class AddPackages {
-        static RemoveRequest Request;
-        static int idx = 0;
-        static string[] packagesToRemove = {
+        static ListRequest ListRequest;
+        static AddAndRemoveRequest AddAndRemoveRequest;
+        static string[] _unwantedPackages = {
             "com.unity.feature.development",
             "com.unity.visualscripting",
             "com.unity.collab-proxy",
@@ -16,38 +20,59 @@ namespace Lotec.Utils.Packages {
             "com.unity.test-framework",
             "com.unity.timeline",
         };
-        static string[] packagesToAdd = {
+        static string[] _requiredPackages = {
             "com.unity.ide.vscode",
         };
 
-        [MenuItem("Tools/Lotec/Set Packages")]
+        [MenuItem("Tools/Lotec/Cleanup Packages")]
         static void SetPackages() {
-            // Unity does NOT complete other requests if something fails???
-            // Request = Client.AddAndRemove(packagesToAdd, packagesToRemove);
-            RemovePackage();
             EditorApplication.update += Progress;
-        }
-
-        static void RemovePackage() {
-            if (idx >= packagesToRemove.Length) {
-                idx = 0;
-                Client.Add(packagesToAdd[0]);
-                return;
-            }
-
-            Request = Client.Remove(packagesToRemove[idx]);
-            EditorApplication.update += Progress;
+            ListRequest = Client.List(true);
         }
 
         static void Progress() {
-            if (Request.IsCompleted) {
-                if (Request.Status != StatusCode.Success)
-                    Debug.Log(Request.Error.message);
-
-                EditorApplication.update -= Progress;
-                idx++;
-                RemovePackage();
+            if (ListRequest != null) {
+                HandleListRequest();
             }
+            if (AddAndRemoveRequest != null) {
+                HandleAddAndRemove();
+            }
+        }
+
+        static void HandleListRequest() {
+            if (!ListRequest.IsCompleted) return;
+
+            if (ListRequest.Status == StatusCode.Success) {
+                PackageCollection packages = ListRequest.Result;
+                string[] packagesToRemove = packages
+                    .Where(package => _unwantedPackages.Contains(package.name))
+                    .Select(package => package.name)
+                    .ToArray();
+                string[] packagesToAdd = packages
+                    .Where(package => _requiredPackages.Contains(package.name))
+                    .Select(package => package.name)
+                    .ToArray();
+                if (packagesToRemove.Length > 0)
+                    Debug.Log($"Removing packages: {System.String.Join(", ", packagesToRemove)}");
+                if (packagesToAdd.Length > 0)
+                    Debug.Log($"Adding packages: {System.String.Join(", ", packagesToAdd)}");
+                AddAndRemoveRequest = Client.AddAndRemove(packagesToAdd, packagesToRemove);
+            } else {
+                Debug.Log(ListRequest.Error.message);
+            }
+
+            EditorApplication.update -= Progress;
+            ListRequest = null;
+        }
+
+        static void HandleAddAndRemove() {
+            if (!AddAndRemoveRequest.IsCompleted) return;
+
+            if (AddAndRemoveRequest.Status != StatusCode.Success)
+                Debug.Log(AddAndRemoveRequest.Error.message);
+
+            EditorApplication.update -= Progress;
+            AddAndRemoveRequest = null;
         }
     }
 }
