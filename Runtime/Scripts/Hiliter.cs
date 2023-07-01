5f105c29fb397ee5bd8e6e99fa8daee80fb757eb
fresolina <74408325+fresolina@users.noreply.github.com>
2023-07-01 16:18:58 +0200

    Add safeguards and update style

--- Runtime/Scripts/Hiliter.cs
+++ Runtime/Scripts/Hiliter.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 
 namespace Lotec.Utils {
     public static class Hiliter {
-        private static int _baseColorId = Shader.PropertyToID("_BaseColor");
-        private static int _colorId = Shader.PropertyToID("_Color");
-        private static MaterialPropertyBlock _hiliteBlock;
+        static readonly int s_baseColorId = Shader.PropertyToID("_BaseColor");
+        static readonly int s_colorId = Shader.PropertyToID("_Color");
+        static MaterialPropertyBlock s_hiliteBlock;
 
         /// <summary>
         /// Run once at application start.
@@ -19,9 +19,9 @@ namespace Lotec.Utils {
         /// </summary>
         /// <param name="hiliteColor"></param>
         public static void InitHilite(Color hiliteColor) {
-            _hiliteBlock = new MaterialPropertyBlock();
-            _hiliteBlock.SetColor(_colorId, hiliteColor);
-            _hiliteBlock.SetColor(_baseColorId, hiliteColor);
+            s_hiliteBlock = new MaterialPropertyBlock();
+            s_hiliteBlock.SetColor(s_colorId, hiliteColor);
+            s_hiliteBlock.SetColor(s_baseColorId, hiliteColor);
         }
 
         /// <summary>
@@ -31,13 +31,18 @@ namespace Lotec.Utils {
         /// <param name="obj">Object to hilite</param>
         /// <param name="on">Turn on/off hilite</param>
         public static void Hilite(GameObject obj, bool on) {
-            if (!obj) return;
+            if (!obj)
+                return;
 
-            var renderers = obj.GetComponentsInChildren<Renderer>();
-            if (renderers.Length == 0) return;
+            Renderer[] renderers = obj.GetComponentsInChildren<Renderer>();
+            if (renderers.Length == 0)
+                return;
+
+            if (s_hiliteBlock == null)
+                InitHilite();
 
             for (int i = 0; i < renderers.Length; i++) {
-                renderers[i].SetPropertyBlock(on ? _hiliteBlock : null);
+                renderers[i].SetPropertyBlock(on ? s_hiliteBlock : null);
             }
         }
 
@@ -48,7 +53,9 @@ namespace Lotec.Utils {
         /// <param name="obj">Object to hilite</param>
         /// <param name="on">Turn on/off hilite</param>
         public static void Hilite(Component obj, bool on) {
-            Hilite(obj?.gameObject, on);
+            if (obj == null)
+                return;
+            Hilite(obj.gameObject, on);
         }
     }
 }
